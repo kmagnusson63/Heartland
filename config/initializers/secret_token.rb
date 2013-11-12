15c4c06df2983936261554eba63ce6708303b0ae
Ken Magnusson <kmagnusson63@academic.rrc.ca>
2013-11-12 07:37:30 -0600

    First commit for ecommercec site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heartland::Application.config.secret_token = '2fd55a8297d88e70d0a01b10267f357e5f24707f014c41b4aa2c2f5b66baf309d4949b0003c450015f19a316d21209bfa158288bbdf0d69ed3282f8589388c82'
